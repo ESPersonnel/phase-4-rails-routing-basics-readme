baa23efe622a81e544b75abc04f1a62c192edc3e
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-09-25 23:22:12 +0300

    Done/

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,36 @@
+class CheesesController < ApplicationController
+    def index
+        cheeses = Cheese.all
+        render json: cheeses
+    end
+    # def index
+    #     cheeses = Cheese.all.order(:name)
+    #     render json: cheeses
+    # end
+    # def index
+    #     cheeses = Cheese.all.limit(3)
+    #     render json: cheeses
+    # end
+    def show
+        cheese = Cheese.find(params[:id])
+        render json: cheese
+    end
+    def create
+        cheese = Cheese.create(cheese_params)
+        render json: cheese
+    end
+    def update
+        cheese = Cheese.find(params[:id])
+        cheese.update(cheese_params)
+        render json: cheese
+    end
+    def destroy
+        cheese = Cheese.find(params[:id])
+        cheese.destroy
+        render json: cheese
+    end
+    # private
+    # def cheese_params
+    #     params.permit(:name, :price, :image_url)
+    # end
+end
